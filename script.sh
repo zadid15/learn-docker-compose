81d633bc725c82156fe7a6505c448680c48f896c
zadid15 <muhammadzadid061512@gmail.com>
2025-06-23 21:16:35 +0700

    Learn Docker Compose

---
+++ script.sh
@@ -0,0 +1,11 @@
+# How to create container with Docker Compose
+docker compose create
+# How to run a container with Docker Compose
+docker compose start
+# How to preview a container with Docker Compose
+docker compose ps
+docker compose ps -a 
+# How to stop a container with Docker Compose
+docker compose stop
+# How to remove a container with Docker Compose
+docker compose down
